ccf6934d1692dc5a47c2380516915a59005e2b46
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2024-05-13 13:18:14 +0000

    update: created fastapi server + folder cleanup

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import cv2
+from ultralytics import YOLO
+import easyocr
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import uvicorn
+
+
+app = FastAPI()
+
+model = YOLO('best.torchscript', task='detect')
+reader = easyocr.Reader(['en'])
+
+@app.post("/detect-license-plate/")
+async def detect_license_plate(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(BytesIO(contents))
+    
+    results = model(np.array(image))
+    plates = []
+
+    for result in results:
+        bbox = result.boxes.xyxy[0]
+        xmin, ymin, xmax, ymax = map(int, bbox)
+        
+        # crop image to license plate only
+        plate_image = np.array(image.crop((xmin, ymin, xmax, ymax)).convert('L'))
+        _, plate_threshold = cv2.threshold(plate_image, 64, 255, cv2.THRESH_BINARY_INV)
+        
+        # read the license plate
+        detections = reader.readtext(plate_threshold)
+        if detections:
+            text, confidence = detections[0][1].upper().replace(' ', ''), detections[0][2]
+            plates.append({'plate': text, 'confidence': confidence})
+
+    return JSONResponse(content={"results": plates})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
